977bf8e8e58c75b8cc1347310b94419e8d049e1b
unknown <borang.3006@yandex.ru>
2022-01-30 15:34:02 +0300

    Connect services to rabbitmq

--- CommandsApi/Program.cs
+++ CommandsApi/Program.cs
@@ -1,7 +1,10 @@
+using CommandApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddMassTransitConnection(builder.Configuration);
 var app = builder.Build();
 
 app.MapControllers();
--- PlatformApi/Program.cs
+++ PlatformApi/Program.cs
@@ -1,7 +1,10 @@
+using PlatformApi.Extension;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddMassTransitConnection(builder.Configuration);
 var app = builder.Build();
 
 app.MapControllers();
